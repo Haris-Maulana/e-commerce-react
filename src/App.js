3a6cd448d98b3be160d6914362312f2bad367188
unknown <harismaulanaslm@gmail.com>
2022-11-13 19:48:59 +0700

    change things up

--- src/App.js
+++ src/App.js
@@ -8,27 +8,24 @@ import Whatsapp from "./component/03-Whatsapp/Whatsapp";
 import Footer from "./component/04-Footer/Footer.js";
 
 function App() {
-  
   const [cart, setCart] = useState([]);
-  const [cartNumber, setCartNumber] = useState(0)
+  const [cartNumber, setCartNumber] = useState(0);
   const cartFilter = cart.filter((item, Title) => cart.indexOf(item) === Title);
 
- 
-
   const handleClick = (itemlist) => {
-    cart.push(itemlist)
+    cart.push(itemlist);
     itemlist.quantity += 1;
     setCartNumber(cartNumber + 1);
-    console.log(cart)
+    console.log(cart);
   };
 
   return (
     <div className="App">
-      <Navbar cartNumber={cartNumber} cart={cart} cartFilter={cartFilter}/>
+      <Navbar cartNumber={cartNumber} cart={cart} cartFilter={cartFilter} />
       <Header />
       <Main handleClick={handleClick} title={"Best Item"} />
-      <Whatsapp />
       <Main handleClick={handleClick} title={"Popular Item"} />
+      <Whatsapp />
       <Footer />
     </div>
   );
